7223277fc13dccb9cd08593a49bd18d4d35763f2
Deepak <deepak664337@gmail.com>
2026-07-04 22:58:29 +0530

    Dom revised

---
+++ lecture10dom/second.js
@@ -0,0 +1,15 @@
+const obj = {
+    name: "Rohit",
+    age: 20,
+    address: {
+        city: "dwarka",
+        pincode: 23321
+    }
+}
+
+
+
+
+
+// docuemnt.getElementById("btn")--IT IS THE METHOD in docuemnt to get that html element it is converted into object form not as html form okay
+
